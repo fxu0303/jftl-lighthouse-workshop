c0e35d327a603dae81d418b45c6c47ac63d754bf
FENG XU <f.xu@revers.io>
2024-06-10 16:40:54 +0200

    up

---
+++ lighthouserc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    ci: {
+        collect: {
+            url: ['http://192.168.1.2:8080'],
+            //url: ['https://aferreira-deo.github.io/jftl/’], 
+        },
+        assert: {
+            preset: 'lighthouse:recommended',
+        },
+        upload: {
+            target: 'temporary-public-storage'
+        },
+    },
+};
